fce9f1559c84a72c9666273599314c18fa316a1b
Illia Ilin <sunrise.th13@gmail.com>
2020-10-02 22:06:20 +0300

    Header Background Fade On Scroll

---
+++ headerBackgroundFade/script.js
@@ -0,0 +1,6 @@
+let headerBg = document.getElementById("bg");
+window.addEventListener("scroll", function () {
+  headerBg.style.opacity = 1 - +window.pageYOffset / 550 + "";
+  headerBg.style.top = +window.pageYOffset + "px";
+  headerBg.style.backgroundPositionY = -+window.pageYOffset / 2 + "px";
+});
