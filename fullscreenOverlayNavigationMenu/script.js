2c10c1485e8bb292a2383bc64c8073f7b080d3e3
Illia Ilin <sunrise.th13@gmail.com>
2020-10-04 13:56:02 +0300

    Fullscreen Overlay Navigation Menu

---
+++ fullscreenOverlayNavigationMenu/script.js
@@ -0,0 +1,6 @@
+function menuToggle() {
+  let nav = document.getElementById("menu-overlay");
+  nav.classList.toggle("active");
+  let icon = document.getElementById("toggleIcon");
+  icon.classList.toggle("active");
+}
